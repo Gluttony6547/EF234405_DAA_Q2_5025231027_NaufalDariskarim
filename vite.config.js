c0543bba6fd75fc371e963a482610be620b3e9c0
Naufal Dariskarim <naufaldariskarim@gmail.com>
2026-05-15 19:32:42 +0700

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/EF234405_DAA_Q2_5025231027_NaufalDariskarim/',
   plugins: [react()],
 })
